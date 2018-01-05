edfb54d9ee7f0602e0e976954d870cd734fbadbf
Rubbertjuh <brend_smits@hotmail.com>
2018-01-05 14:39:20 +0100

    Create SQLStatements class and set up connection.

---
+++ src/main/java/net/toastynetworks/beaconwatch/SQLStatements.java
@@ -0,0 +1,12 @@
+package net.toastynetworks.beaconwatch;
+
+import net.kaikk.mc.kaiscommons.sql.SQLConnection;
+
+public class SQLStatements {
+	
+	public SQLConnection connection;
+	public SQLStatements(SQLConnection connection) {
+		this.connection = connection;
+	}
+
+}
